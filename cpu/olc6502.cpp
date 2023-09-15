41206c92b5476fe931e75a3d358e07bc95fe7565
jmrossi98 <jmrossi98@gmail.com>
2023-09-15 16:44:49 -0400

    Started with CPU implementation

---
+++ cpu/olc6502.cpp
@@ -0,0 +1,16 @@
+#include "olc6502.h"
+#include "Bus.h"
+
+olc6502::olc6502(){}
+
+olc6502::~olc6502(){}
+
+uint8_t olc6502::read(uint16_t a)
+{
+    return bus->read(a, false);
+}
+
+void olc6502::write(uint16_t a, uint8_t d)
+{
+    return bus->write(a, d);
+}
