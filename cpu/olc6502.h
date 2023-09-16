17d23f88f514655f6fef5e72935d8a42aefd32cb
jmrossi98 <jmrossi98@gmail.com>
2023-09-15 22:18:19 -0400

    Added definitions of 6502 CPU registers and addressing modes

--- cpu/olc6502.h
+++ cpu/olc6502.h
@@ -21,10 +21,28 @@ public:
         N = (1 << 7), // Negative
     };
 
+    uint8_t a = 0x00; // Accumulator register
+    uint8_t x = 0x00; // X Register
+    uint8_t y = 0x00; // Y Register
+    uint8_t stkp = 0x00; // Stack Pointer (points location on bus)
+    uint16_t pc = 0x0000; // Program counter
+    uint8_t status = 0x00; // Status Register
+
     void ConnectBus(Bus *n) {bus = n;}
+
+    // Addressing Modes
+    uint8_t IMP(); uint8_t IMM();
+    uint8_t ZP0(); uint8_t ZPX();
+    uint8_t ZPY(); uint8_t REL();
+    uint8_t ABS(); uint8_t ABX();
+    uint8_t ABY(); uint8_t IND();
+    uint8_t IZX(); uint8_t IZY();
 
 private:
     Bus *bus = nullptr;
     uint8_t read(uint16_t a);
     void write(uint16_t a, uint8_t d);
+
+    uint8_t GetFlag(FLAGS6502 f);
+    void SetFlag(FLAGS6502 f, bool v);
 };
