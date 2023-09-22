7dafc922f9d2c1bd6820ce8c30fb922c78adf28c
jmrossi98 <jmrossi98@gmail.com>
2023-09-21 22:21:44 -0400

    Stubbing out case switch statements for ppu read/write functions

--- src/ppu/olc2C02.cpp
+++ src/ppu/olc2C02.cpp
@@ -1,8 +1,61 @@
 #pragma once
 
-class olc2C02
+#include "olc2C02.h"
+
+olc2C02::olc2C02()
 {
-private:
-    olc2C02();
-    ~olc2C02();
-};
+}
+
+olc2C02::~olc2C02()
+{
+}
+
+uint8_t olc2C02::cpuRead(uint16_t addr, bool rdonly)
+{
+	uint8_t data = 0x00;
+
+	switch (addr)
+	{
+	case 0x0000: // Control
+		break;
+	case 0x0001: // Mask
+		break;
+	case 0x0002: // Status
+		break;
+	case 0x0003: // OAM Address
+		break;
+	case 0x0004: // OAM Data
+		break;
+	case 0x0005: // Scroll
+		break;
+	case 0x0006: // PPU Address
+		break;
+	case 0x0007: // PPU Data
+		break;
+	}
+
+	return data;
+}
+
+void olc2C02::cpuWrite(uint16_t addr, uint8_t data)
+{
+	switch (addr)
+	{
+	case 0x0000: // Control
+		break;
+	case 0x0001: // Mask
+		break;
+	case 0x0002: // Status
+		break;
+	case 0x0003: // OAM Address
+		break;
+	case 0x0004: // OAM Data
+		break;
+	case 0x0005: // Scroll
+		break;
+	case 0x0006: // PPU Address
+		break;
+	case 0x0007: // PPU Data
+		break;
+	}
+}
