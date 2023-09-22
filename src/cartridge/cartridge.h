0ca38a6ce15fc8635527ba3f634275b41478fb89
jmrossi98 <jmrossi98@gmail.com>
2023-09-21 23:33:20 -0400

    Fixed cartridge cpp implementation, added some helper vars and cartridge initialization

--- src/cartridge/cartridge.h
+++ src/cartridge/cartridge.h
@@ -1,11 +1,26 @@
 #pragma once
 
+#include <cstdint>
+#include <string>
+#include <vector>
+
 class Cartridge
 {
 public:	
 	Cartridge();
 	~Cartridge();
 
+private:
+    std::vector<uint8_t> vPRFMemory;
+    std::vector<uint8_t> vCHRMemory;
+    
+    uint8_t nMapperID = 0;
+    uint8_t nPRGBanks = 0;
+    uint8_t nCHRBanks = 0;
+
+    std::shared_ptr<Mapper> pMapper;
+
+public:
     // Main bus comms
     uint8_t cpuRead(uint16_t addr, bool rdonly = false);
     void cpuWrite(uint16_t addr, uint8_t data);
