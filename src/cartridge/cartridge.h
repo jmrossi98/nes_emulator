3b01de43c3bd70931acded6a1701f2afdf1f831c
jmrossi98 <jmrossi98@gmail.com>
2023-09-21 22:21:10 -0400

    Added functions for CPU/PPU read/write in cartridge header

--- src/cartridge/cartridge.h
+++ src/cartridge/cartridge.h
@@ -5,4 +5,12 @@ class Cartridge
 public:	
 	Cartridge();
 	~Cartridge();
+
+    // Main bus comms
+    uint8_t cpuRead(uint16_t addr, bool rdonly = false);
+    void cpuWrite(uint16_t addr, uint8_t data);
+
+    // PPU bus comms
+    uint8_t ppuRead(uint16_t addr, bool rdonly = false);
+    void ppuWrite(uint16_t addr, uint8_t data);
 };
