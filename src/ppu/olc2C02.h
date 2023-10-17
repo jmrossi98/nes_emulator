40ea1c088c1a236e9180a8c4c78d0e4a30a9722a
jmrossi98 <jmrossi98@gmail.com>
2023-10-17 16:30:24 -0400

    Implemented ppu read/write/clock methods in ppu class, some variables for keeping track of table pattern/cycles

--- src/ppu/olc2C02.h
+++ src/ppu/olc2C02.h
@@ -17,6 +17,11 @@ private:
 
     uint8_t tblName[2][1024];
     uint8_t tblPalette [32];
+	uint8_t tblPattern[2][4096];
+
+private:
+	int16_t scanline = 0;
+	int16_t cycle = 0;
 
 public:
     // Main bus comms
