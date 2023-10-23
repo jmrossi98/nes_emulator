9ee34b11f9379c1db5029161915564ce8574d258
jmrossi98 <jmrossi98@gmail.com>
2023-10-23 15:20:34 -0400

    Define pattern table/color methods in header

--- src/ppu/olc2C02.h
+++ src/ppu/olc2C02.h
@@ -4,6 +4,7 @@
 #include <memory>
 
 #include "../cartridge/Cartridge.h"
+#include "../../lib/olcPixelGameEngine.h"
 
 class olc2C02
 {
@@ -16,12 +17,28 @@ private:
     std::shared_ptr<Cartridge> cart;
 
     uint8_t tblName[2][1024];
-    uint8_t tblPalette [32];
+    uint8_t tblPalette[32];
 	uint8_t tblPattern[2][4096];
 
 private:
 	int16_t scanline = 0;
 	int16_t cycle = 0;
+
+ private:
+	olc::Pixel  palScreen[0x40];
+
+	olc::Sprite* sprScreen;
+	olc::Sprite* sprNameTable[2];
+	olc::Sprite* sprPatternTable[2];   
+
+public:
+	olc::Sprite& GetScreen();
+	olc::Sprite& GetNameTable(uint8_t i);
+	olc::Sprite& GetPatternTable(uint8_t i, uint8_t palette);
+
+	olc::Pixel& GetColourFromPaletteRam(uint8_t palette, uint8_t pixel);
+
+	bool frame_complete = false;
 
 public:
     // Main bus comms
