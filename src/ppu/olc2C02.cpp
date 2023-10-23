02b74f9c745a57a788760bd2f8d0c8db732ab043
jmrossi98 <jmrossi98@gmail.com>
2023-10-23 15:19:38 -0400

    Added method for getting pattern table and color in ppu

--- src/ppu/olc2C02.cpp
+++ src/ppu/olc2C02.cpp
@@ -91,3 +91,52 @@ void olc2C02::ConnectCartridge(const std::shared_ptr<Cartridge>& cartridge)
 void olc2C02::clock()
 {
 }
+
+olc::Sprite& olc2C02::GetPatternTable(uint8_t i, uint8_t palette)
+{
+	// Planes are stored as 8 bytes of LSB, followed by 8 bytes of MSB. Loop through all 16x16 tiles
+	for (uint16_t nTileY = 0; nTileY < 16; nTileY++)
+	{
+		for (uint16_t nTileX = 0; nTileX < 16; nTileX++)
+		{
+			// Convert the 2D tile coordinate into a 1D offset into the pattern table memory.
+			uint16_t nOffset = nTileY * 256 + nTileX * 16;
+
+			// Now loop through 8 rows of 8 pixels
+			for (uint16_t row = 0; row < 8; row++)
+			{
+
+				// Read from pattern tables
+				uint8_t tile_lsb = ppuRead(i * 0x1000 + nOffset + row + 0x0000);
+				uint8_t tile_msb = ppuRead(i * 0x1000 + nOffset + row + 0x0008);
+
+				// Iterate through the 8-bit words, combining them to give us the final pixel index
+				for (uint16_t col = 0; col < 8; col++)
+				{
+					uint8_t pixel = (tile_lsb & 0x01) + (tile_msb & 0x01);
+
+					// Shift the row words 1 bit right for each column of the character.
+					tile_lsb >>= 1; tile_msb >>= 1;
+
+					// We can translate to a screen colour, and coordinates of the sprite
+					sprPatternTable[i]->SetPixel
+					(
+						nTileX * 8 + (7 - col),
+						nTileY * 8 + row, 
+						GetColourFromPaletteRam(palette, pixel)
+					);
+				}
+			}
+		}
+	}
+
+	return *sprPatternTable[i];
+}
+
+olc::Pixel& olc2C02::GetColourFromPaletteRam(uint8_t palette, uint8_t pixel)
+{
+	// Gives appropriate screen colour based on a specified palette and pixel index.
+	return palScreen[ppuRead(0x3F00 + (palette << 2) + pixel) & 0x3F];
+}
+
+
