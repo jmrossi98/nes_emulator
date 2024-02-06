5eec25ae6e2a80327e39172f246896d4d512536b
jmrossi98 <jmrossi98@gmail.com>
2024-02-05 22:11:24 -0500

    Implemented audio functionality in main emulator class

--- main.cpp
+++ main.cpp
@@ -1,26 +1,44 @@
 #include <iostream>
 #include <sstream>
+#include <deque>
 
 #include "Bus.h"
-#include "olc6502.h"
 
 #define OLC_PGE_APPLICATION
+#define OLC_PGEX_SOUND
 #include "olcPixelGameEngine.h"
+#include "olcPGEX_Sound.h"
 
-class Demo_olc2C02 : public olc::PixelGameEngine
+class Emulator : public olc::PixelGameEngine
 {
 public:
-	Demo_olc2C02() { sAppName = "RETRoNES"; }
+	Emulator() { sAppName = "RETRoNES"; }
 	std::string rom_url;
 
 private: 
-	// The NES
 	Bus nes;
 	std::shared_ptr<Cartridge> cart;
 	bool bEmulationRun = true;
 	float fResidualTime = 0.0f;
+	uint8_t nSelectedPalette = 0x00;
+	std::list<uint16_t> audio[4];
+	float fAccumulatedTime = 0.0f;
 
-	bool OnUserCreate()
+private:
+	// The NES
+	static Emulator* instance;
+	static float SoundOut(int nChannel, float fGlobalTime, float fTimeStep)
+	{
+		if (nChannel == 0)
+		{
+			while (!instance->nes.clock()) {};
+			return static_cast<float>(instance->nes.dAudioSample);
+		}
+		else
+			return 0.0f;
+	}
+
+	bool OnUserCreate() override
 	{
 		// Load the cartridge
 		cart = std::make_shared<Cartridge>(rom_url);
@@ -31,13 +49,43 @@ private:
 		// Insert into NES
 		nes.insertCartridge(cart);
 
+		for (int i = 0; i < 4; i++)
+		{			
+			for (int j = 0; j < 120; j++)
+				audio[i].push_back(0);
+		}
+
 		// Reset NES
 		nes.reset();
+
+		// Initialize PGEX sound system
+		instance = this;
+		nes.SetSampleFrequency(44100);
+		olc::SOUND::InitialiseAudio(44100, 1, 8, 512);
+		olc::SOUND::SetUserSynthFunction(SoundOut);
 		return true;
 	}
 
-	bool OnUserUpdate(float fElapsedTime)
+	bool OnUserDestroy() override
 	{
+		olc::SOUND::DestroyAudio();
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		fAccumulatedTime += fElapsedTime;
+		if (fAccumulatedTime >= 1.0f / 60.0f)
+		{
+			fAccumulatedTime -= (1.0f / 60.0f);
+			audio[0].pop_front();
+			audio[0].push_back(nes.apu.pulse1_visual);
+			audio[1].pop_front();
+			audio[1].push_back(nes.apu.pulse2_visual);
+			audio[2].pop_front();
+			audio[2].push_back(nes.apu.noise_visual);
+		}
+
 		Clear(olc::BLACK);
 
 		// Handle input for controller
@@ -55,25 +103,18 @@ private:
 		if (GetKey(olc::Key::R).bPressed) nes.reset();
 		if (GetKey(olc::Key::ESCAPE).bPressed) return false;
 
-		if (fResidualTime > 0.0f)
-			fResidualTime -= fElapsedTime;
-		else
-		{
-			fResidualTime += (1.0f / 60.0f) - fElapsedTime;
-			do { nes.clock(); } while (!nes.ppu.frame_complete);
-			nes.ppu.frame_complete = false;
-		}
-
 		DrawSprite(131, 0, &nes.ppu.GetScreen(), 2);
 		return true;
 	}
 };
 
+Emulator* Emulator::instance = nullptr;
+
 int main(int argc, char* argv[])
 {
-	Demo_olc2C02 demo;
-	demo.rom_url = argv[1];
-	demo.Construct(780, 480, 2, 2, true);
-	demo.Start();
+	Emulator emulator;
+	emulator.rom_url = argv[1];
+	emulator.Construct(780, 480, 2, 2);
+	emulator.Start();
 	return 0;
 }
